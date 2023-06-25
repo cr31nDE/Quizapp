3bd93c26760731956514738543e312fcff2c3e36
Jonas Gersting <jonasgersting@web.de>
2023-06-25 18:31:30 +0200

    question card

---
+++ script.js
@@ -0,0 +1,37 @@
+let questtions = [
+    {
+        "question1": "was ist 1 + 1?",
+        "answer1.1": "2",
+        "answer2.1": "3",
+        "answer3.1": "4",
+        "answer4.1": "5",
+    },
+    {
+        "question1": "was ist 1 + 1?",
+        "answer1.1": "2",
+        "answer2.1": "3",
+        "answer3.1": "4",
+        "answer4.1": "5",
+    },
+    {
+        "question1": "was ist 1 + 1?",
+        "answer1.1": "2",
+        "answer2.1": "3",
+        "answer3.1": "4",
+        "answer4.1": "5",
+    },
+    {
+        "question1": "was ist 1 + 1?",
+        "answer1.1": "2",
+        "answer2.1": "3",
+        "answer3.1": "4",
+        "answer4.1": "5",
+    },
+    {
+        "question1": "was ist 1 + 1?",
+        "answer1.1": "2",
+        "answer2.1": "3",
+        "answer3.1": "4",
+        "answer4.1": "5",
+    },
+]
